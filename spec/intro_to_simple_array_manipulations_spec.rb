58962ca552a337345f8708631cb47f39123988c4
Zipporah Horch <zipporahhorch@gmail.com>
2020-11-04 23:17:31 +0000

    Automatically backed up by Learn

--- spec/intro_to_simple_array_manipulations_spec.rb
+++ spec/intro_to_simple_array_manipulations_spec.rb
@@ -1,6 +1,5 @@
 require './lib/intro_to_simple_array_manipulations.rb'
 
-
 describe "using_push" do 
   it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
     colors_in_the_rainbow = ["red", "orange", "yellow", "green", "blue", "indigo"]
